fd2e235de1126aa4cdd30d1cdf43bf61d1186ff0
ren kato <ren.k90729@icloud.com>
2025-04-12 12:10:48 +0900

    Add /callback endpoint for LINE Webhook

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def index():
+    return "LINE Translate Bot is running!" 
+
+@app.route("/callback", methods=["POST"]) 
+def callback():
+    print("Webhook受信！")           
+    return "OK", 200  
+
 # 環境変数から取得（後でRenderで設定）
 LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
