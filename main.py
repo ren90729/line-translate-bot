a72adf58864ee89266f63d3da42fcd4a82ccf2b9
ren kato <ren.k90729@icloud.com>
2025-04-12 12:46:48 +0900

    Add /callback endpoint for LINE Webhookss

--- main.py
+++ main.py
@@ -1,57 +1,38 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import requests
-import os
 
 app = Flask(__name__)
 
-# 環境変数から取得（後でRenderで設定）
-LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
-DEEPL_API_KEY = os.environ.get('DEEPL_API_KEY')
+# ↓ LINE Developer Console から取得
+LINE_CHANNEL_SECRET = "YOUR_CHANNEL_SECRET"
+LINE_CHANNEL_ACCESS_TOKEN = "YOUR_CHANNEL_ACCESS_TOKEN"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-def translate_to_english(text):
-    url = 'https://api-free.deepl.com/v2/translate'
-    data = {
-        'auth_key': DEEPL_API_KEY,
-        'text': text,
-        'target_lang': 'EN'
-    }
-    response = requests.post(url, data=data)
-    return response.json()['translations'][0]['text']
-
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers.get("X-Line-Signature")
     body = request.get_data(as_text=True)
-
-@app.route("/", methods=["GET"])
-def index():
-    return "LINE Translate Bot is running!" 
+    app.logger.info("Request body: " + body)
 
     try:
         handler.handle(body, signature)
-    except Exception as e:
-        print("Error:", e)
+    except InvalidSignatureError:
+        app.logger.warning("Invalid signature.")
         abort(400)
 
-    return 'OK'
+    return 'OK', 200
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    original_text = event.message.text
-    translated_text = translate_to_english(original_text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=translated_text)
+        TextSendMessage(text=event.message.text)
     )
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)        
+    app.run(port=5000)
+  
