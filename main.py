61389a483cf4a0bf34ebebbf0a8f32bc198aa580
ren kato <ren.k90729@icloud.com>
2025-04-12 13:14:31 +0900

    Add /callback endpoint for LINE Webhookss

--- main.py
+++ main.py
@@ -38,3 +38,6 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+print(f"Signature: {signature}")
+print(f"Body: {body}")
